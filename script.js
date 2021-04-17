9040a968563ef22af7c88995a3717e22db3f1d54
BBrownley <brendobrownley@yahoo.com>
2021-04-17 06:53:40 -0400

    Added scrolling project images

--- script.js
+++ script.js
@@ -26,3 +26,78 @@ const darkModeHandler = (() => {
 
   init();
 })();
+
+const ImageScrollHandler = (() => {
+  // Target images to be scrolled
+
+  const designoImages = document.querySelectorAll(
+    ".project-designo > div > div"
+  );
+
+  const designoMobileImg = document.querySelector(
+    ".project-designo .outline-mobile div"
+  );
+
+  const designoTabletImg = document.querySelector(
+    ".project-designo .outline-tablet div"
+  );
+
+  const designoDesktopImg = document.querySelector(
+    ".project-designo .outline-desktop div"
+  );
+
+  const designoinitialPositions = {
+    mobile: parseInt(
+      window
+        .getComputedStyle(designoMobileImg)
+        .getPropertyValue("background-position-y")
+        .split("px")[0]
+    ),
+    tablet: parseInt(
+      window
+        .getComputedStyle(designoTabletImg)
+        .getPropertyValue("background-position-x")
+        .split("px")[0]
+    ),
+    desktop: parseInt(
+      window
+        .getComputedStyle(designoDesktopImg)
+        .getPropertyValue("background-position-y")
+        .split("px")[0]
+    )
+  };
+
+  // Scroll
+
+  let increment = 0;
+  let scrolling = true;
+
+  const scroll = () => {
+    if (!scrolling) return false;
+    designoMobileImg.style.backgroundPositionY = `-${designoinitialPositions.mobile +
+      increment}px`;
+    designoTabletImg.style.backgroundPositionX = `${designoinitialPositions.tablet +
+      increment}px`;
+    designoDesktopImg.style.backgroundPositionY = `-${designoinitialPositions.desktop +
+      increment}px`;
+    increment += 1;
+  };
+
+  setInterval(scroll, 50);
+
+  // Stop scrolling whenever user hovers over one of the images
+
+  designoImages.forEach(image => {
+    image.addEventListener("mouseenter", () => {
+      if (scrolling) {
+        clearInterval(scroll);
+        scrolling = false;
+      }
+    });
+    image.addEventListener("mouseleave", () => {
+      if (scrolling === false) {
+        scrolling = true;
+      }
+    });
+  });
+})();
